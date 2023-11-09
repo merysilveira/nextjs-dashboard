1ffeeab86b8c65aeec50290614804cdd39548ab8
Meriellen Silveira <merysilveira@gmail.com>
2023-11-09 12:20:25 -0300

    Image's remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nextjs-dashboard-omega-tan.vercel.app',
+      },
+    ],
+  },
+}
